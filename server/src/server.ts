c48b0973c4ba5513ab5a285234b917dd9564472b
arthur-wronski <91790901+arthur-wronski@users.noreply.github.com>
2024-12-17 15:07:10 +0000

    [FEAT] basic deployment endpoint

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,31 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 
 const app = express();
-const port = 3000;
+const port = 3001;
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, Express + TypeScript!');
+});
+
+app.post('/api/deploy', (req: Request, res: Response)=> {
+  const { primaryChain, secondaryChain, functionToCopy, contract } = req.body;
+
+  if (!primaryChain || !secondaryChain || !functionToCopy || !contract) {
+    res.status(400).json({ message: 'Missing required fields' });
+  }
+
+  console.log('Deployment request received:', {
+    primaryChain,
+    secondaryChain,
+    functionToCopy,
+    contract,
+  });
+
+  res.status(200).json({ message: 'Deployment successful' });
 });
 
 app.listen(port, () => {
