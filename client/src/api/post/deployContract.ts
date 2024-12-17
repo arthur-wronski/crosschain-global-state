a25747dc685319e3b3d8778a7ff419cb19846e47
arthur-wronski <91790901+arthur-wronski@users.noreply.github.com>
2024-12-17 17:06:56 +0000

    [FEAT] added deployContract api handler

---
+++ client/src/api/post/deployContract.ts
@@ -0,0 +1,22 @@
+import { handleApiRequest } from "@/api/apiHandler"
+
+export const deployContract = async (primaryChain: string, secondaryChain: string, functionToCopy: string, contract: string) => {
+  try {
+
+    const params = {primaryChain, secondaryChain, functionToCopy, contract}
+
+    const result = await handleApiRequest({
+      url: "/api/deploy",
+      tag: `deployContract`,
+      method: "POST",
+      body: params,
+      successMessage: "Contract Deployment Successful",
+      errorMessage: "Contract Deployment Failed",
+    })
+
+    return result
+  } catch (error) {
+    console.error("Deployment error:", error)
+    return null
+  }
+}
