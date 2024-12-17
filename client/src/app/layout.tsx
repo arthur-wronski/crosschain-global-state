2e333385192725facf07fe8220121ebd33a6338a
arthur-wronski <91790901+arthur-wronski@users.noreply.github.com>
2024-12-17 17:06:17 +0000

    [FEAT] added toaster to root layout

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
+import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
   title: "Orbitum",
@@ -16,6 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <NavBar/>
+        <Toaster />
         {children}
       </body>
     </html>
