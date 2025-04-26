4534fde82f1498be0ea03ba003f219394f8877c2
arthur-wronski <91790901+arthur-wronski@users.noreply.github.com>
2025-04-26 12:26:34 +0100

    [CHORE] change func name

--- server/src/helpers/generateProxyContract.ts
+++ server/src/helpers/generateProxyContract.ts
@@ -1,4 +1,4 @@
-export function generateProxyContractCode(functionSignature: string): string {
+export function generateProxyContract(functionSignature: string): string {
   return `
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
